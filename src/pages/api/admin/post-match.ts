3dfa07ebd993f8fad4afe6528aff1d9413c4c393
Philip Carlsson <philipcarlsson@hotmail.com>
2023-02-16 21:17:47 +0100

    creating BE call for creating match and installing tanstack react query

--- src/pages/api/admin/post-match.ts
+++ src/pages/api/admin/post-match.ts
@@ -1,12 +1,31 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient } from "mongodb";
 
 type Data = {
-  name: string;
+  match: any; // for now
+  message: string;
 };
 
-const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  res.status(200).json({ name: "John Doe" });
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(process.env.MONGODB_URI as string);
+
+    const db = client.db();
+
+    const matchesCollection = db.collection("matches");
+
+    const matches = await matchesCollection.insertOne(data);
+
+    console.log(matches);
+
+    client.close();
+
+    res.status(201).json({ message: "Match created.", match: data });
+
+    return data;
+  }
 };
 
 export default handler;
