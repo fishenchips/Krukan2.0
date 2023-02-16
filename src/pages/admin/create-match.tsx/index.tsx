95fd382f2129b4161b8bc65126b9953bb9349968
Philip Carlsson <philipcarlsson@hotmail.com>
2023-02-16 22:10:55 +0100

    post request works, match stored in DB

--- src/pages/admin/create-match.tsx/index.tsx
+++ src/pages/admin/create-match.tsx/index.tsx
@@ -1,5 +1,19 @@
+import { useCreateMatch } from "@/queries/matches/hooks/useCreateMatch";
+
 const CreateMatch = () => {
-  return <></>;
+  const { mutate } = useCreateMatch({
+    home: true,
+    arena: "zinken",
+    date: "23-03-16, 16:00",
+    gameType: "friendly",
+    opposition: "Stureby",
+  });
+
+  const handleCreateMatch = () => {
+    mutate();
+  };
+
+  return <button onClick={handleCreateMatch}>POST</button>;
 };
 
 export default CreateMatch;
--- src/queries/matches/hooks/useCreateMatch.ts
+++ src/queries/matches/hooks/useCreateMatch.ts
@@ -2,10 +2,7 @@ import { useMutation, UseMutationOptions } from "@tanstack/react-query";
 import { Match } from "../types";
 import { createMatch } from "../match-queries";
 
-export const useCheckCreateBrawlExpression = (
-  match: Match,
-  options: UseMutationOptions
-) =>
+export const useCreateMatch = (match: Match, options?: UseMutationOptions) =>
   useMutation({
     mutationFn: () => createMatch(match),
     ...options,
