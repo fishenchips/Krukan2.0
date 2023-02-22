308b306df8189dbdb6c55d8ba5006c195d7384f2
Philip Carlsson <philipcarlsson@hotmail.com>
2023-02-22 15:29:33 +0100

    installing chakra toast

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,12 +4,14 @@ import { SignInButton } from "./SignInButton";
 import { SignOutButton } from "./SignOutButton";
 
 export const Header = () => {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+
+  console.log({ status });
 
   return (
     <header className={styles.header}>
       <div className={styles.banner}>
-        <h3>FC Krukan</h3>
+        <h3>FC Krukan - Lirarnas Lag </h3>
       </div>
       <div className={styles.userHub}>
         {session ? <SignOutButton /> : <SignInButton />}
