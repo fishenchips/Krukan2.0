afd3f00684a5b4fcc102ee80d156536a85d59c3f
Philip Carlsson <philipcarlsson@hotmail.com>
2023-02-22 15:16:34 +0100

    moving signin and out to header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,32 @@
 import styles from "./Header.module.css";
+import { useSession, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
+import { SignInButton } from "./SignInButton";
 
 export const Header = () => {
+  const { data: session } = useSession();
+  const { push } = useRouter();
+
+  console.log(session);
+
+  const handleSignOut = async () => {
+    const data = await signOut({ redirect: false, callbackUrl: "/logged-out" });
+
+    push(data.url);
+  };
+
   return (
     <header className={styles.header}>
-      <div>
+      <div className={styles.banner}>
         <h3>FC Krukan</h3>
       </div>
-      <div></div>
+      <div className={styles.userHub}>
+        {session ? (
+          <button onClick={handleSignOut}>Sign out</button>
+        ) : (
+          <SignInButton />
+        )}
+      </div>
     </header>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,9 @@
 import { UserLandingPage } from "@/components/users/UserLandingPage";
-import { useSession, signOut } from "next-auth/react";
-import { useRouter } from "next/router";
 
 const Home = () => {
-  const { data: session } = useSession();
-
-  const { push, asPath } = useRouter();
-
-  console.log(session);
-
-  const handleSignOut = async () => {
-    const data = await signOut({ redirect: false, callbackUrl: "/logged-out" });
-
-    push(data.url);
-  };
-
-  const handleSignIn = () => {
-    push(`/auth/signin?callbackUrl=${asPath}`);
-  };
-
   return (
     <div>
-      {session ? (
-        <>
-          <button onClick={handleSignOut}>Sign out</button>
-          <UserLandingPage />
-        </>
-      ) : (
-        <>
-          <button onClick={handleSignIn}>Sign in</button>
-        </>
-      )}
+      <UserLandingPage />
     </div>
   );
 };
