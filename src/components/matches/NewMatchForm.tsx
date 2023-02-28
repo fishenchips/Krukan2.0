1900d463a3cab468193a9e4ef9326f1a9bbd9bcd
Philip Carlsson <philipcarlsson@hotmail.com>
2023-02-28 20:33:16 +0100

    creating new match works

--- src/components/matches/NewMatchForm.tsx
+++ src/components/matches/NewMatchForm.tsx
@@ -1,57 +1,68 @@
 import { Switch } from "@chakra-ui/react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 import { useCreateMatch } from "@/queries/matches/hooks/useCreateMatch";
+import { Match } from "@/queries/matches/types";
 
 export const NewMatchForm = () => {
+  const { register, handleSubmit, watch } = useForm<Match>();
+
   const { mutate } = useCreateMatch({
-    home: true,
-    arena: "zinken",
-    date: "23-03-16",
-    time: "16:00",
-    gameType: "friendly",
-    opposition: "Stureby",
+    home: watch("home"),
+    arena: watch("arena"),
+    opposition: watch("opposition"),
+    date: watch("date"),
+    time: watch("time"),
+    gameType: watch("gameType"),
   });
 
-  const handleCreateMatch = () => {
+  const onSubmit: SubmitHandler<Match> = () => mutate();
+
+  /*  const handleCreateMatch = () => {
     mutate();
-  };
+  }; */
 
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="isHomeMatch">Home match (true/false)</label>
-          <Switch id="isHomeMatch" size="lg" colorScheme="red" isRequired />
+          <Switch
+            id="isHomeMatch"
+            size="lg"
+            colorScheme="red"
+            isRequired
+            {...register("home")}
+          />
         </div>
         <div>
           <label htmlFor="arena">Arena</label>
-          <input id="arena" type="text" />
+          <input id="arena" type="text" {...register("arena")} />
         </div>
         <div>
           <label htmlFor="opposition">Opposition</label>
-          <input id="opposition" type="text" />
+          <input id="opposition" type="text" {...register("opposition")} />
         </div>
         <div>
           <label htmlFor="date">Date</label>
-          <input id="date" type="date" />
+          <input id="date" type="date" {...register("date")} />
         </div>
         <div>
           <label htmlFor="time">Time</label>
-          <input id="time" type="time" />
+          <input id="time" type="time" {...register("time")} />
         </div>
         <div>
           <label htmlFor="type">Game type</label>
-          <select defaultValue={""} id="type">
+          <select defaultValue={""} id="type" {...register("gameType")}>
             <option disabled value="">
               --choose game type--
             </option>
             <option value="friendly">friendly</option>
-            <option value="league match">league</option>
+            <option value="league">league</option>
           </select>
         </div>
+        <button type="submit">POST</button>
       </form>
-
-      <button onClick={handleCreateMatch}>POST</button>
     </>
   );
 };
--- src/queries/matches/match-queries.ts
+++ src/queries/matches/match-queries.ts
@@ -4,7 +4,7 @@ import type { Match } from "./types";
 export const createMatch = async (enteredMatchData: Match) => {
   const response = await fetch("/api/admin/post-match", {
     method: "POST",
-    body: JSON.stringify({ enteredMatchData }),
+    body: JSON.stringify(enteredMatchData),
     headers: {
       "Content-Type": "application/json",
     },
