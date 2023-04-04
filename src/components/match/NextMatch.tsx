b66cfbb7d498e5c01b1dd2bfc5e64397f06034e2
Philip Carlsson <philipcarlsson@hotmail.com>
2023-04-04 16:36:05 +0200

    creating component for next match, using dummy data

---
+++ src/components/match/NextMatch.tsx
@@ -0,0 +1,41 @@
+import { useGetNextMatch } from "@/queries/matches/hooks/useGetNextMatch";
+import { Loading } from "../layout/Loading";
+import dayjs from "dayjs";
+import Link from "next/link";
+
+const DUMMY_DATA = {
+  _id: "ets",
+  home: true,
+  arena: "Zinken",
+  date: new Date().toISOString(),
+  opposition: "Ljungbackens IF",
+  gameType: "league",
+  roster: [2, 2, 2, 2],
+};
+
+export const NextMatch = () => {
+  /* const { data: nextMatch, isLoading, isError } = useGetNextMatch();
+
+  if (isLoading) return <Loading />;
+
+  if (isError) return <></>; */
+
+  return (
+    <div>
+      <h3>Next Match:</h3>
+      <div>
+        <p>
+          {DUMMY_DATA.home
+            ? `Krukan - ${DUMMY_DATA.opposition}`
+            : `${DUMMY_DATA.opposition} - Krukan`}
+        </p>
+        <p>
+          {dayjs(DUMMY_DATA.date).format("dddd, D/M HH:mm")} at{" "}
+          {DUMMY_DATA.arena}
+        </p>
+        <p>Available players: {DUMMY_DATA.roster.length}</p>
+        <Link href={`/matches/${DUMMY_DATA._id}`}>Go to match</Link>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import { LandingPage } from "@/components/layout/Home";
+import { NextMatch } from "@/components/match/NextMatch";
 import { UserLandingPage } from "@/components/users/UserLandingPage";
 import { useSession } from "next-auth/react";
 
 const Home = () => {
   const { data: session } = useSession();
 
-  return <div>{session ? <UserLandingPage /> : <LandingPage />}</div>;
+  return (
+    <div>
+      {session ? <UserLandingPage /> : <LandingPage />} <NextMatch />
+    </div>
+  );
 };
 
 export default Home;
