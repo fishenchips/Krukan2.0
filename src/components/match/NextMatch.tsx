0fb4b2c0fe680eb679696f9fdb57b9db561e9c65
Philip Carlsson <philipcarlsson@hotmail.com>
2023-04-06 16:54:29 +0200

    replacing dummy data with db data

--- src/components/match/NextMatch.tsx
+++ src/components/match/NextMatch.tsx
@@ -4,38 +4,27 @@ import dayjs from "dayjs";
 import Link from "next/link";
 import styled from "./NextMatch.module.css";
 
-const DUMMY_DATA = {
-  _id: "ets",
-  home: true,
-  arena: "Zinken",
-  date: new Date().toISOString(),
-  opposition: "Ljungbackens IF",
-  gameType: "league",
-  roster: [2, 2, 2, 2],
-};
-
 export const NextMatch = () => {
-  /* const { data: nextMatch, isLoading, isError } = useGetNextMatch();
+  const { data: nextMatch, isLoading, isError } = useGetNextMatch();
 
   if (isLoading) return <Loading />;
 
-  if (isError) return <></>; */
+  if (isError) return <></>;
 
   return (
     <div className={styled.nextMatch}>
       <h3>Next Match:</h3>
       <div>
         <p>
-          {DUMMY_DATA.home
-            ? `Krukan - ${DUMMY_DATA.opposition}`
-            : `${DUMMY_DATA.opposition} - Krukan`}
+          {nextMatch.home
+            ? `Krukan - ${nextMatch.opposition}`
+            : `${nextMatch.opposition} - Krukan`}
         </p>
         <p>
-          {dayjs(DUMMY_DATA.date).format("dddd, D/M HH:mm")} at{" "}
-          {DUMMY_DATA.arena}
+          {dayjs(nextMatch.date).format("dddd, D/M HH:mm")} at {nextMatch.arena}
         </p>
-        <p>Available players: {DUMMY_DATA.roster.length}</p>
-        <Link href={`/matches/${DUMMY_DATA._id}`}>Go to match</Link>
+        <p>Available players: {nextMatch.roster?.length}</p>
+        <Link href={`/matches/${nextMatch._id}`}>Go to match</Link>
       </div>
     </div>
   );
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -2,35 +2,52 @@ import { Loading } from "@/components/layout/Loading";
 import { useGetMatches } from "@/queries/matches/hooks/useGetMatches";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { useState } from "react";
 
 const AdminPage = () => {
+  /* store in cookies or local storage so they can enter again? */
+  const [password, setPassword] = useState<string>("");
   const { data: session, status } = useSession();
 
   const { data: matches, isLoading } = useGetMatches();
 
-  if (status === "unauthenticated") return <p>Access denied.</p>;
+  /*   if (status === "unauthenticated") return <p>Access denied.</p>;
 
   if (isLoading) {
     return <Loading />;
-  }
+  } */
+  console.log(process.env.ADMIN_PASSWORD);
 
   return (
     <div>
-      <p>Welcome to admin page, {session?.user?.email}</p>
       <div>
-        <Link href={"/admin/create-match"}>Create new match</Link>
-        <div>
-          <h5>Update match</h5>
-          {matches &&
-            matches?.map((match) => (
-              <div key={match._id}>
-                <Link href={`/admin/update-match/${match._id}`}>
-                  {match.opposition} - {match.date}
-                </Link>
-              </div>
-            ))}
-        </div>
+        <label htmlFor="admin">Enter Password</label>
+        <input
+          id="admin"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
+
+      {password === "coachseger" && (
+        <>
+          <p>Welcome to admin page, {session?.user?.email}</p>
+          <div>
+            <Link href={"/admin/create-match"}>Create new match</Link>
+            <div>
+              <h5>Update match</h5>
+              {matches &&
+                matches.map((match) => (
+                  <div key={match._id}>
+                    <Link href={`/admin/update-match/${match._id}`}>
+                      {match.opposition} - {match.date}
+                    </Link>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
