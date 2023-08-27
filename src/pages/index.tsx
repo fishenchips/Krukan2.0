d063c7b9f394f77d353a324a4b5130ac27d38d0b
fishenchips <philipcarlsson@hotmail.com>
2023-08-27 14:58:38 +0200

    adding Head data landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,30 @@
+import Head from "next/head";
+import { useSession } from "next-auth/react";
+
 import { LandingPage } from "@/components/layout/Home";
 import { SocialMedia } from "@/components/layout/SocialMedia";
 import { NextMatch } from "@/components/match/NextMatch";
 import { UserLandingPage } from "@/components/users/UserLandingPage";
-import { useSession } from "next-auth/react";
 
 const Home = () => {
   const { data: session } = useSession();
 
   return (
-    <div>
-      {session ? <UserLandingPage /> : <LandingPage />}
-      <NextMatch />
-      <SocialMedia />
-    </div>
+    <>
+      <Head>
+        <title>FC Krukan - Lirarnas Lag</title>
+        <meta
+          name="description"
+          content="Welcome to the hub of FC Krukan. Create a user to be able to attend future matches!"
+          key="desc"
+        />
+      </Head>
+      <div>
+        {session ? <UserLandingPage /> : <LandingPage />}
+        <NextMatch />
+        <SocialMedia />
+      </div>
+    </>
   );
 };
 
