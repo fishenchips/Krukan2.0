ee964d05ffbba2bba91962f3aac2a3e618f41a34
fishenchips <philipcarlsson@hotmail.com>
2024-02-06 21:27:43 +0100

    update type

--- src/queries/users/hooks/useGetAllPlayers.ts
+++ src/queries/users/hooks/useGetAllPlayers.ts
@@ -1,12 +1,12 @@
 import { UseQueryOptions, useQuery } from "@tanstack/react-query";
 import { getPlayers } from "../user-queries";
-import { LeaderBoardPlayers, Player } from "@/utils/types/playerInfo";
+import { LeaderBoardPlayer } from "@/utils/types/playerInfo";
 import { ApiError } from "next/dist/server/api-utils";
 
 export const playersKey = "players";
 
 export const useGetAllPlayers = (
-  options?: UseQueryOptions<unknown, ApiError, LeaderBoardPlayers>
+  options?: UseQueryOptions<unknown, ApiError, LeaderBoardPlayer>
 ) =>
   useQuery({
     queryKey: [playersKey],
