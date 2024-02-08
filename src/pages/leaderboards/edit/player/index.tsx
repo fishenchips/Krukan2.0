1cee9e6e2207d93d3468d58e0d9635a6d787a946
fishenchips <philipcarlsson@hotmail.com>
2024-02-08 21:59:15 +0100

    adding password to change leaderboard

--- src/pages/leaderboards/edit/player/index.tsx
+++ src/pages/leaderboards/edit/player/index.tsx
@@ -1,6 +1,29 @@
+import { Loading } from "@/components/layout/Loading";
 import { PlayerList } from "@/components/layout/PlayerList";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import cookieCutter from "cookie-cutter";
+import { AdminLogin } from "@/components/admin/login";
 
 const EditPlayerLeaderboardPage = () => {
-  return <PlayerList />;
+  const [password, setPassword] = useState<string | undefined>("");
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <Loading />;
+  }
+
+  if (status === "unauthenticated") return <p>Access denied.</p>;
+
+  return (
+    <>
+      {password === process.env.NEXT_PUBLIC_ADMIN_PASSWORD ||
+      cookieCutter.get("isAdmin") ? (
+        <PlayerList />
+      ) : (
+        <AdminLogin setPassword={setPassword} />
+      )}
+    </>
+  );
 };
 export default EditPlayerLeaderboardPage;
